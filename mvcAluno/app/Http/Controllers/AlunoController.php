1b31387e763e69b3895b6b8303a1336cfb53895f
mickael <mickael.ezequiel@aluno.senai.br>
2026-03-23 10:42:38 -0300

    tela de atualizar

--- mvcAluno/app/Http/Controllers/AlunoController.php
+++ mvcAluno/app/Http/Controllers/AlunoController.php
@@ -1,16 +1,52 @@
 <?php
 
-// estou no alunoController.php
 namespace App\Http\Controllers;
 
 use App\Models\Aluno;
-use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+
 
 class AlunoController extends Controller
 {
-    public function listar(){
-        $query = Aluno::query();
-        $alunos = $query->get();
+    public function listar()
+    {
+        $alunos = Aluno::all();
         return view('listar', compact('alunos'));
+    }
+
+    public function add(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|'
+        ]);
+
+        Aluno::create([
+            'nome' => $request->nome,
+            'email' => $request->email
+        ]);
+
+        return redirect()->back()->with('success', 'Aluno cadastrado com sucesso!');
+    }
+
+    public function atualizar($id){
+        $aluno = Aluno::findOrFail($id);
+        return view('atualizar', compact('aluno'));
+    }
+
+    public function update(Request $request, $id){
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => "required|string|max:255|unique:alunos,amail,$id"
+        ]);
+
+        $aluno = Aluno::findOrFail($id);
+
+        $aluno->nome = $request->nome;
+        $aluno->email = $request->email;
+
+
+        $aluno->save();
+        return redirect()->back()->with('succes' , 'Aluno atualizado com sucesso');
     }
 }
